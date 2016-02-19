02862f189a3a239e15f58ac28d002a2eeaa51ced
abutygin27 <*>
2016-02-19 17:22:05 +0300

    sysout

--- src/com/banana/test/Test.java
+++ src/com/banana/test/Test.java
@@ -3,6 +3,7 @@ package com.banana.test;
 public class Test {
 
     public static void main(String[] args) {
+        System.out.println("Test!!!");
         MessageProvider messageProvider = new MessageProviderImpl();
         MessageRenderer messageRenderer = new MessageRendererImpl();
         messageRenderer.render(messageProvider);
